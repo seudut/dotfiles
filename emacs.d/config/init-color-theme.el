09a27f206951958452a9232ca26a169823c4af1d
Peng Li <seudut@gmail.com>
2015-05-14 20:33:34 +0800

    emacs: change file directories, using init.el instead of .emacs, create config folder for other elisp files

---
+++ emacs.d/config/init-color-theme.el
@@ -0,0 +1,33 @@
+
+;(load-theme 'tango-dark)
+
+(require 'color-theme)
+(load-theme 'sanityinc-tomorrow-bright t)
+
+;(require 'moe-theme)
+;(moe-dark)
+
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
+(require 'powerline)
+;(setq powerline-arrow-shape 'arrow14) ;; best for small fonts
+
+;(require 'smart-mode-line)
+;; These two lines are just examples
+;(setq powerline-arrow-shape 'curve)
+;(setq powerline-default-separator-dir '(right . left))
+;; These two lines you really need.
+;(setq sml/theme 'powerline)
+;(sml/setup)
+;(if after-init-time (sml/setup)
+;  (add-hook 'after-init-hook 'sml/setup))
+
+
+;;---------------------------------------------------------------------------------------
+;; powerline
+;;
+
+;;;;(require 'powerline)
+;;;;(powerline-evil-center-color-theme)
+;(powerline-evil-vim-color-theme)
+
+(provide 'init-color-theme)
