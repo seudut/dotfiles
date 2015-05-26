ffce0bc4f2e2fbc46d5bc79757a292edbc9306a6
Peng Li <seudut@gmail.com>
2015-05-27 00:34:48 +0800

    emacs: ggtags config, using ido-completion, and split vertically

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -163,17 +163,21 @@
 ;;;; show default directory on mode-line
 (ggtags-mode 1)
 
+;; using ido-completion for ggtags
+;; https://github.com/leoliu/ggtags/issues/56
+(setq ggtags-completing-read-function
+      (lambda (&rest args)
+        (apply #'ido-completing-read
+               (car args)
+               (all-completions "" ggtags-completion-table)
+               (cddr args))))
 
 
-;;j;(setq ggtags-completing-read-function
-;;j;      (lambda (&rest args)
-;;j;        (apply #'ido-completing-read
-;;j;               (car args)
-;;j;               (all-completions "" ggtags-completion-table)
-;;j;               (cddr args))))
 
 
 ;; gnu global support
 ;(require 'semantic/db)
 ;(global-semanticdb-minor-mode 1)
 
+(custom-set-variables
+ '(ggtags-split-window-function (quote split-window-vertically)))
