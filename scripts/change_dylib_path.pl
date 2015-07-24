3f99ddc1302d9c08a36417bb869a06442acca0cb
Peng Li <seudut@gmail.com>
2015-07-24 15:15:08 +0800

    add install_name_tool change executable path

---
+++ scripts/change_dylib_path.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl -w
+#
+use strict;
+use warnings;
+#use v5.10;
+
+my @cpve_libs = ( 'libpme.0.dylib', 'libpmexy.1.dylib', 'libpmpxy.1.dylib', 'libtaf.0.dylib', 'libtafxy.1.dylib','libsrtp.dylib' );
+
+my @lib_to_change = ( 'libxml2.2.dylib', 'libssl.dylib', 'libcrypto.dylib' );
+
+for my $ff (@cpve_libs){
+    !system '/usr/bin/install_name_tool -change @executable_path/../Frameworks/' . $_ . ' @executable_path/' . $_ . ' ./' . $ff or die "$!"     for @lib_to_change;
+}
+
+print  "==DONE==" . "\n";
+
