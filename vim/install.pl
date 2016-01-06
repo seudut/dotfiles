231be6a3b816216f8bad19249c1f93d29ce861a6
Peng Li <seudut@gmail.com>
2016-01-06 11:44:27 +0800

    vim: change install.pl

--- vim/install.pl
+++ vim/install.pl
@@ -6,7 +6,11 @@ my $vundle_url = "https://github.com/gmarik/Vundle.vim.git";
 chdir "bundle";
 system "git clone " . $vundle_url;
 
-## 2. install pacage
+## 2. install packages
+#
+#
+# 3. install YouCompleteMe
+# ./install.py --clang-completer
 
 
 #vim --cmd 'echo "aaaa"';
