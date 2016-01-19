69e526851394f67681a6c97e6f310d9cad92d808
Peng Li <seudut@gmail.com>
2016-01-19 13:05:24 +0800

    emacs - add org init file

---
+++ emacs.d/elisp/init-org.el
@@ -0,0 +1,13 @@
+
+
+;; http://doc.norang.ca/org-mode.html  Org Mode - Organize Your Life In Plain Text!
+
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
+(require 'org)
+
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+
+(provide 'init-org)
