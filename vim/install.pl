98d89d74764047a2cabea2195da37abdd990a160
Peng Li <seudut@gmail.com>
2016-01-28 13:35:38 +0800

    vim - improve install.pl

--- vim/install.pl
+++ vim/install.pl
@@ -1,16 +1,27 @@
 #!/usr/bin/perl -w
 
+my $vim_git = $ENV{'PWD'};
+my $vim_dir = "$ENV{'HOME'}/.vim";
+my $vim_rc = "$ENV{'HOME'}/.vimrc";
+
+## test if vim and vimrc exist
+die "~/.vim or ~/.vimrc exists!!, backup them first\n" if -e $vim_dir or -e $vim_rc ;
+
+
+## add new files	
+print `ln -s $vim_git $vim_dir`;
+print `ln -s $vim_dir/vimrc $vim_rc`;
+
 ## 1. git clone package manager Vundle
 my $vundle_url = "https://github.com/gmarik/Vundle.vim.git";
- 
+
 chdir "bundle";
 system "git clone " . $vundle_url;
 
-## 2. install packages
+##
+## then start vim, and run :BundleInstall to install other packagesj
 #
 #
 # 3. install YouCompleteMe
 # ./install.py --clang-completer
 
-
-#vim --cmd 'echo "aaaa"';
