579def2a8889452497961079085304fd06640f41
Peng Li <seudut@gmail.com>
2016-06-22 23:53:31 +0800

    emacs - set custom.el file

--- emacs.d_2/init.el
+++ emacs.d_2/init.el
@@ -6,22 +6,3 @@
 (package-initialize)
 
 (org-babel-load-file "~/.emacs.d/config.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (counsel slime ido-exit-target undo-tree gnugo emms which-key ido-ubiquitous persp-projectile projectile company company-mode lispy worf use-package smex smartparens smart-comment org-bullets org multi-term magit linum-relative ido-vertical-mode flx-ido el-get color-theme-sanityinc-tomorrow color-theme color-identifiers-mode ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(mode-line-buffer-id ((t (:foreground "black" :bold t))))
- '(org-block ((t (:background "#000000"))))
- '(org-block-background ((t (:background "#000000"))))
- '(org-block-begin-line ((t (:foreground "#008ED1" :background "#002E41"))))
- '(org-block-end-line ((t (:foreground "#008ED1" :background "#002E41"))))
- '(which-func ((t (:foreground "green")))))
