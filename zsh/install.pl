13c5ade6dc04b91c00b64d4a0464d3b5def1d5b4
Peng Li <seudut@gmail.com>
2017-02-18 15:50:44 +0800

    add install file for zsh

---
+++ zsh/install.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl -w
+
+use strict;
+use File::Basename;
+use Cwd;
+use Cwd "abs_path";
+
+my $home = $ENV{'HOME'};
+my $zshDir = dirname abs_path(__FILE__);
+my $zshrc = $zshDir . "/zshrc";
+
+die "~/.zsh or ~/.zshrc exist. Backup them first.\n" if -e "$home/.zsh" or -e "$home/.zshrc";
+
+print `ln -s $zshDir $home/.zsh`;
+print `ln -s $zshrc $home/.zshrc`;
+
+print "Done" unless $?;
+
