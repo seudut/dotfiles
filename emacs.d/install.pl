a6221472b3ff2a3992aeb44fb70dad09f5bc6113
Peng Li <seudut@gmail.com>
2017-03-03 23:58:10 +0800

    emacs - fix install file bug

--- emacs.d/install.pl
+++ emacs.d/install.pl
@@ -10,8 +10,8 @@ use Cwd "abs_path";
 my $home = $ENV{'HOME'};
 my $emacsDir = dirname abs_path (__FILE__);
 
-die "~/.emacs.d or ~/init.el exist. Backup them first.\n" 
-    if -e "$home/.init.el" or -e "$home/.emacs.d";
+die "~/.emacs.d or ~/.init.el or ~/.emacs exist. Backup them first.\n" 
+    if -e "$home/.init.el" or -e "$home/.emacs.d" or -e "$home/.emacs";
 
 print `ln -s $emacsDir $home/.emacs.d`;
 
