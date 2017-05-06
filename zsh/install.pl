c69d522b6ccf78a3029cccf627361a6c5449483d
debian <debian@debian>
2017-05-06 23:22:12 +0800

    zshrc - fix installation issue

--- zsh/install.pl
+++ zsh/install.pl
@@ -11,21 +11,19 @@ my $zshrc = $zshDir . "/zshrc";
 
 die "~/.zsh or ~/.zshrc exist. Backup them first.\n" if -e "$home/.zsh" or -e "$home/.zshrc";
 
-print `ln -s $zshDir $home/.zsh`;
-print `ln -s $zshrc $home/.zshrc`;
 
 my $zshsyntaxDir = $zshDir . "plugins/zsh-syntax-highlighting";
 
 if (not -e $zshsyntaxDir) {
-    die "zsh syntax highlighting not exist, abort.\n";
-} elsif (-s $zshsyntaxDir) {
     print " clone submoduells\n ";
-    chdir $zshsyntaxDir;
     ! system "git submodule init" or die $!;
     ! system "git submodule update" or die $!;
 } else {
     print "zsh syntax submodule already exists.\n";
 }
+
+print `ln -s $zshDir $home/.zsh`;
+print `ln -s $zshrc $home/.zshrc`;
 
 print "Done\n" unless $?;
 
