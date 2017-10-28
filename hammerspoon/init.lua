937131850a4324bbf8bbf2cf108375ce124f3bd1
Peng Li <seudut@gmail.com>
2017-10-28 13:31:03 +0800

    Emacs - change irony to ycmd

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -24,42 +24,6 @@ appWatcher = hs.application.watcher.new(applicationWatcher)
 appWatcher:start()
 
 
-
----- Sample - Drawing on the screen - http://www.hammerspoon.org/go/#simplereload
---mouseCircle = nil
---mouseCircleTimer = nil
---function mouseHighlight()
---    -- Delete an existing highlight if it exists
---    if mouseCircle then
---        mouseCircle:delete()
---        if mouseCircleTimer then
---            mouseCircleTimer:stop()
---        end
---    end
---    -- Get the current co-ordinates of the mouse pointer
---    mousepoint = hs.mouse.getAbsolutePosition()
---    -- Prepare a big red circle around the mouse pointer
---    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
---    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
---    mouseCircle:setFill(false)
---    mouseCircle:setStrokeWidth(5)
---    mouseCircle:show()
---
---    -- Set a timer to delete the circle after 3 seconds
---    mouseCircleTimer = hs.timer.doAfter(3, function() mouseCircle:delete() end)
---end
---hs.hotkey.bind({"cmd","alt","shift"}, "D", mouseHighlight)
---
---
----- window movement
---hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
---  local win = hs.window.focusedWindow()
---  local f = win:frame()
---
---  f.x = f.x - 10
---  win:setFrame(f)
---end)
-
 -- Window Movement
 -- https://andrich.blog/2016/11/20/hammerspoon-an-awesome-tool-to-automate-your-mac/
 -- CTRL + ALT + Left - Move current window to the left half of the screen.
@@ -160,6 +124,7 @@ INSTALL:andUse("TextClipboardHistory",
                {
                   config = {
                      show_in_menubar = false,
+                     hist_size = 500,
                   },
                   hotkeys = {
                      toggle_clipboard = { { "cmd", "shift" }, "v"}
